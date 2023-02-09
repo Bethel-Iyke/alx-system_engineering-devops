b65087e910c2607bd937099ada01e32822450fab
Bethek-Iyke <talk2bethel017@gmail.com>
2023-02-09 21:13:19 +0100

    web debug4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,9 @@
 # Increase the limit of open file descriptors
 
 exec { 'increase_open_file_limit':
-  provider => shell,
-  command  => 'sed -i "56s|5|65536|" /etc/security/limits.conf',
-  before   => Exec['increase_open_file_limit'],
+  command => 'sed -i "56s|5|65536|" /etc/security/limits.conf \
+	   && sed -i "57s|4|65536|" /etc/security/limits.conf \
+	   && echo "session required pam_limits.so" >> /etc/pam.d/common-session',
+  path    => ['/bin','/usr/bin']
 }
 
-exec { 'increase_open_file_limit':
-  provider => shell,
-  command  => 'sed -i "57s|4|65536|" /etc/security/limits.conf',
-}
